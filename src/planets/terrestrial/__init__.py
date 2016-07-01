9cfe88650f70c7e98e0b8479201b04710918eaba
Marco Sirabella <msirabel@gmail.com>
2016-07-01 18:46:36 -0400

    - Erased all traces of terplanet

--- src/planets/terrestrial/__init__.py
+++ src/planets/terrestrial/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from . import physiography, tectonics, terplanet
+from . import physiography, tectonics
 from ...misc import shapes, edits
 
 __appname__    = "Ex-Nihilo"

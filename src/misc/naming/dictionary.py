c133f282507505f28530ff1ec803a4e1b469286a
Marco Sirabella <msirabel@gmail.com>
2016-07-04 11:02:44 -0400

    - Primitive random name generation

---
+++ src/misc/naming/dictionary.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__appname__    = "Ex-Nihilo"
+__author__     = "Marco Sirabella"
+__copyright__  = ""
+__credits__    = ["Marco Sirabella"]  # Authors and bug reporters
+__license__    = "GPL 3.0"
+__version__    = "0.3.0"
+__maintainer__ = "Marco Sirabella"
+__email__      = "msirabel@gmail.com"
+__status__     = "Prototype"
+__module__     = ""
+
+vowels = (
+    "a",
+    "e",
+    "ee",
+    "i",
+    "o",
+    "u",
+)
+
+constonants = (
+    "qu",
+    "w",
+    "s",
+    "t",
+    "g",
+    "m",
+    "c",
+    "f",
+    "r",
+    "b",
+    "tr",
+    "d",
+    "f",
+    "g",
+    "h",
+    "sh",
+    "th",
+    "gr",
+    "ch",
+    "gl",
+)
+
+#syllables = (
---
+++ src/misc/naming/generate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import dictionary
+import random
+
+__appname__    = "Ex-Nihilo"
+__author__     = "Marco Sirabella"
+__copyright__  = ""
+__credits__    = ["Marco Sirabella"]  # Authors and bug reporters
+__license__    = "GPL 3.0"
+__version__    = "0.3.0"
+__maintainer__ = "Marco Sirabella"
+__email__      = "msirabel@gmail.com"
+__status__     = "Prototype"
+__module__     = ""
+
+def generate():
+    length = random.randrange(2,10)
+    name = ""
+    y = None
+    for x in range(0, length):
+        if y == "vowel" or y == None:
+            if random.random() > 0.25:
+                name += dictionary.constonants[random.randrange(0, len(dictionary.constonants))]
+                y = "constonant"
+            else:
+                name += dictionary.vowels[random.randrange(0, len(dictionary.vowels))]
+                y = "vowel"
+        else:
+            name += dictionary.vowels[random.randrange(0, len(dictionary.vowels))]
+            y = "vowel"
+    return(name)
